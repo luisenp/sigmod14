4cf3d7b7f2272cd650b3505afdd7396c4d908080
luisenp <luisen.p@gmail.com>
2014-03-09 20:17:03 -0400

    tester for query2

---
+++ src/test/TestQuery2.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.FileNotFoundException;
+
+import sigmod14.Query2;
+
+public class TestQuery2 {
+	public static void main(String args[]) {
+		Query2 q = new Query2();
+		long time = System.currentTimeMillis();
+		try {
+			q.setup(null, null);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println(System.currentTimeMillis() - time);
+	}
+}
