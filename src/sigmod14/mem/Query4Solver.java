77c46f939bd0fd5a583f5e9676a6eee3ea268c94
luisenp <luisen.p@gmail.com>
2014-04-15 21:54:53 -0400

    initial threaded version of query4

--- src/sigmod14/mem/Query4Solver.java
+++ src/sigmod14/mem/Query4Solver.java
@@ -1,11 +1,18 @@
 package sigmod14.mem;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import sigmod14.mem.graph.NotFoundException;
 import sigmod14.mem.graph.Person;
@@ -16,9 +23,11 @@ public class Query4Solver {
 	private Database db;
 	private LinkedList<String> queries;
 	private HashMap<String,String> answers;
+	private int numThreads;
 	
-	public Query4Solver(Database db) {
+	public Query4Solver(Database db, int numThreads) {
 		this.db = db;
+		this.numThreads = numThreads;
 	}	
 	
 	private class Centrality {
@@ -101,83 +110,136 @@ public class Query4Solver {
 		if (maxC.compare(c) < 0) return false;
 		return true;
 	}
-	
-	public String query4(int k, String tagName) {
-		if (db.getNumPersons() > 11000) return "-1";
-		// finding the tag with the given name
-		Tag tag = null;
-		for (int idTag : db.getAllTags()) {
-			Tag node = db.getTag(idTag);
-			try {
-				if (db.getTagName(idTag).equals(tagName)) {
-					tag = node;
-				}
-			} catch (NotFoundException e) {
-				System.err.println("ERROR: Tag should have a name");
-				e.printStackTrace();
-				System.exit(-1);
-			}
-		}
-		
-		// finding all vertices on the induced graph
-		HashSet<Person> vertices = new HashSet<Person>();
-		for (Person person : tag.getMembersForums()) {
-			vertices.add(person);
-		}
-		Person sortedVertices[] = vertices.toArray(new Person[vertices.size()]);		
-		Arrays.sort(sortedVertices, new PersonDegreeComparator());
-		
-		int n1 = vertices.size() - 1;
-	
-		// from each node p on the graph, do a BFS and compute centrality
-		PriorityQueue<PersonCentrality> pq =
-			new PriorityQueue<PersonCentrality> 
-				(k + 1, new PersonCentralityComparator());
-		int cnt = 0;
-		for (Person p : sortedVertices) {
-			if (4*cnt++ > vertices.size()) 
-				break;
-			HashSet<Person> visited = new HashSet<Person> ();
-			
-			LinkListInt queue = new LinkListInt(db.getNumPersons());
-			LinkListInt dist = new LinkListInt(db.getNumPersons());
 
-			queue.add(p.getId());
-			dist.add(0);
-			visited.add(p);
-			long rp = -1, sp = 0;
-			// do a BFS to compute relevant quantities rp, sp				
-			while (!queue.isEmpty()) {
-				Person p2 = db.getPerson(queue.removeFirst());
-				int d = dist.removeFirst();
-				// visit only vertices with the given forum tag
-				rp++;
-				sp += d;
-				if (pq.size() >= k
-					&& !checkCentrality(rp, sp, n1, d, pq.peek().centrality)) {
-					// stops if max. possible centrality is lower than the
-					// worst one in the priority queue
-					break;
-				}
-				for (Integer adjPersonID : p2.getKnows().keySet()) {
-					Person adjPerson = db.getPerson(adjPersonID);
-					if (!vertices.contains(adjPerson) || visited.contains(adjPerson)) 
-						continue;
-					visited.add(adjPerson);
-					queue.add(adjPerson.getId());
-					dist.add(d + 1);
-				}
-			}			
-			pq.add(new PersonCentrality(p, new Centrality(rp*rp, n1*sp)));
-			if (pq.size() > k) pq.poll();
+    class BFSOuter implements Callable<PriorityQueue<PersonCentrality>>  {
+    	LinkedList<Person> persons;
+        HashSet<Person> vertices;
+        PriorityQueue<PersonCentrality> pq;
+        int k;
+        int n1;
+        long rp = -1, sp = 0;
+
+        BFSOuter(HashSet<Person> vertices, int k, int n1) {
+        	this.persons = new LinkedList<Person>();
+        	this.pq = 
+        		new PriorityQueue<PersonCentrality> 
+        							(k + 1, new PersonCentralityComparator());
+            this.vertices = vertices;
+            this.k = k;
+            this.n1 = n1;
+        }
+
+        public void addPerson(Person p) {
+        	persons.add(p);
+        }
+        
+        public PriorityQueue<PersonCentrality> call() {
+        	for (Person p : persons) {            	
+            	HashSet<Person> visited = new HashSet<Person>();
+    			LinkListInt queue = new LinkListInt(db.getNumPersons());
+    			LinkListInt dist = new LinkListInt(db.getNumPersons());
+
+    			queue.add(p.getId());
+    			dist.add(0);
+    			visited.add(p);
+    			long rp = -1, sp = 0;
+    			// do a BFS to compute relevant quantities rp, sp				
+    			while (!queue.isEmpty()) {
+    				Person p2 = db.getPerson(queue.removeFirst());
+    				int d = dist.removeFirst();
+    				// visit only vertices with the given forum tag
+    				rp++;
+    				sp += d;
+    				if (pq.size() >= k
+    					&& !checkCentrality(rp, sp, n1, d, pq.peek().centrality)) {
+    					// stops if max. possible centrality is lower than the
+    					// worst one in the priority queue
+    					break;
+    				}
+    				for (Integer adjPersonID : p2.getKnows().keySet()) {
+    					Person adjPerson = db.getPerson(adjPersonID);
+    					if (!vertices.contains(adjPerson) 
+    							|| visited.contains(adjPerson)) 
+    						continue;
+    					visited.add(adjPerson);
+    					queue.add(adjPerson.getId());
+    					dist.add(d + 1);
+    				}
+    			}	
+	            pq.add(new PersonCentrality(p, new Centrality(rp*rp, n1*sp)));
+	            if (pq.size() > k) pq.poll();
+        	}
+        	return pq;
+        }
+    }
+    
+    public String query4(int k, String tagName) {
+        // finding the tag with the given name
+        Tag tag = new Tag(0);
+        for (int idTag : db.getAllTags()) {
+            Tag node = db.getTag(idTag);
+            try {
+                if (db.getTagName(idTag).equals(tagName)) {
+                    tag = node;
+                }
+            } catch (NotFoundException e) {
+                System.err.println("ERROR: Tag should have a name");
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+        // finding all vertices on the induced graph
+        HashSet<Person> vertices = new HashSet<Person>();
+        for (Person person : tag.getMembersForums()) {
+            vertices.add(person);
+        }
+        Person sortedVertices[] = vertices.toArray(new Person[vertices.size()]);        
+        Arrays.sort(sortedVertices, new PersonDegreeComparator());
+
+        int n1 = vertices.size() - 1;
+
+        ExecutorService pool = Executors.newFixedThreadPool(numThreads);
+        ArrayList<BFSOuter> tasks = new ArrayList<BFSOuter>(numThreads);
+        for (int i = 0; i < numThreads; i++) {
+            tasks.add(i, new BFSOuter(vertices, k, n1));
+        }
+        int cnt = 0;
+        for (Person p : sortedVertices) {
+            if (4*cnt > vertices.size()) 
+              break;
+            tasks.get(cnt % numThreads).addPerson(p);
+            cnt++;
+        }
+        PriorityQueue<PersonCentrality> pq =
+                new PriorityQueue<PersonCentrality> 
+                    (k + 1, new PersonCentralityComparator());
+ 
+        try {
+        	List<Future<PriorityQueue<PersonCentrality>>> results = 
+        			pool.invokeAll(tasks);
+	        for (Future<PriorityQueue<PersonCentrality>> r : results) {        	
+				
+					PriorityQueue<PersonCentrality> pqThread = r.get();
+		            pq.addAll(pqThread);
+		            while (pq.size() > k) 
+		            	pq.poll();
+	        }
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
 		}
-		String queryAns = "";
-		while (!pq.isEmpty()) {
-			PersonCentrality pc = pq.poll();
-			queryAns = String.valueOf(pc.person.getId()) + " " + queryAns;
-		}
-		return queryAns;
-	}
+        pool.shutdown();
+        
+        cnt = 0;
+        String queryAns = "";
+        while (cnt < k) {
+            PersonCentrality pc = pq.poll();
+            queryAns = String.valueOf(pc.person.getId()) + " " + queryAns;
+            cnt++;
+        }
+        return queryAns;
+    }
+
 
 	public HashMap<String,String> getAnswers() {
 		return answers;
